d3c23cad5cb70f0d32ffe289bed944886c7071fb
mirlan2020 <mirlan.mederbekov@yandex.ru>
2022-07-11 21:38:39 +0300

    first commit

---
+++ Webinar_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Webinar_test
+//
+//  Created by Mirlan Mederbekov on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
